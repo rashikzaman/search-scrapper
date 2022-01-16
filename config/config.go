e77a529e00e71f575a97d4105d2e72ab873dbba0
Rashik Zaman <rashikzaman13@gmail.com>
2022-01-16 23:21:57 +0600

    Added config and log package

---
+++ config/config.go
@@ -0,0 +1,56 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+var configInstance *Config
+
+type Config struct {
+}
+
+func GetConfig() (config Config) {
+	if configInstance == nil {
+		envFile := os.Getenv("ENV_FILE")
+		if envFile == "" {
+			envFile = ".env"
+		}
+		err := godotenv.Load(envFile)
+		if err != nil {
+			panic("Error loading .env file")
+		}
+		configInstance = new(Config)
+	}
+	return *configInstance
+}
+
+type DatabaseConfig struct {
+	DatabaseName string `json:"database_name"`
+	Host         string `json:"host"`
+	Port         string `json:"port"`
+	Username     string `json:"username"`
+	Password     string `json:"password"`
+}
+
+func (Config) getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
+func (c Config) GetEnvVariable(key string) string {
+	return c.getEnv(key, "")
+}
+
+func (c Config) GetDatabaseConfig() DatabaseConfig {
+	return DatabaseConfig{
+		DatabaseName: c.getEnv("POSTGRES_DB_NAME", "test"),
+		Host:         c.getEnv("POSTGRES_HOST", "localhost"),
+		Port:         c.getEnv("POSTGRES_PORT", "5432"),
+		Username:     c.getEnv("POSTGRES_DB_USERNAME", "test"),
+		Password:     c.getEnv("POSTGRES_DB_PASSWORD", "test"),
+	}
+}
--- go.mod
+++ go.mod
@@ -9,9 +9,11 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/jackc/pgx/v4 v4.14.1 // indirect
 	github.com/jcmturner/gokrb5/v8 v8.2.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/ugorji/go v1.2.6 // indirect
 	golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce // indirect
 	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
